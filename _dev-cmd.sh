2227cd092ad1114e53a267511d7fb8a10ff03a07
Ben Mares <services-git-throwaway1@tensorial.com>
2022-06-14 09:48:50 +0200

    Set git blame ignore when it exists

--- _dev-cmd.sh
+++ _dev-cmd.sh
@@ -33,4 +33,12 @@ if [[ -S /var/run/docker.sock ]] ; then
   fi
 fi
 
+# Set default blame ignore filename.
+# This should only be done when it exists, due to <https://stackoverflow.com/q/70435937>
+if [ -f .git-blame-ignore-revs ]; then
+    git config --system blame.ignoreRevsFile .git-blame-ignore-revs
+fi
+
+echo "Sleeping forever."
+
 while sleep 1000; do :; done
