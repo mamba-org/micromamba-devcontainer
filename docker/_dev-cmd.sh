b4088e6c22a05610757b5da7001b3c132090fb2e
Ben Mares <services-git-throwaway1@tensorial.com>
2022-09-08 21:21:50 +0200

    Move Docker config to a separate script

--- docker/_dev-cmd.sh
+++ docker/_dev-cmd.sh
@@ -5,33 +5,7 @@
 
 set -x
 
-echo "Configuring Docker group..."
-
-# Configure Docker permissions.
-if [[ -S /var/run/docker.sock ]] ; then
-  # Get the GID of the "docker" group.
-  docker_gid=`stat --format=%g /var/run/docker.sock`
-  if [ -z "$docker_gid" ] ; then
-    echo "No mounted Docker socket found."
-  else
-    if getent group "${docker_gid}" ; then
-        # The group for the Docker socket's gid already exists.
-        echo "Adding user to '$(getent group "${docker_gid}" | cut -d: -f1)' group for docker access."
-        sudo usermod -aG "${docker_gid}" "${MAMBA_USER}"
-    else
-        # The group for the Docker socket's gid doesn't exist.
-        if getent group docker ; then
-          # The "docker" group exists, but doesn't match the gid of the Docker socket.
-          docker_group_name="docker-conflicting-groupname"
-        else
-          docker_group_name="docker"
-        fi
-        echo "Setting the GID of the '${docker_group_name}' group to ${docker_gid}."
-        sudo groupadd --force --gid "${docker_gid}" "${docker_group_name}"
-        sudo usermod -aG "${docker_group_name}" "${MAMBA_USER}"
-    fi
-  fi
-fi
+source _configure-docker-group.sh
 
 # Set default blame ignore filename.
 # This should only be done when it exists, due to <https://stackoverflow.com/q/70435937>
