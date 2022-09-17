0f25eb50bf46ba86b0392076556e89e5af17efdf
Ben Mares <services-git-throwaway1@tensorial.com>
2022-09-17 20:28:37 +0200

    Configure docker group for current user, not MAMBA_USER

--- docker/_configure-docker-group.sh
+++ docker/_configure-docker-group.sh
@@ -12,7 +12,7 @@ if [[ -S /var/run/docker.sock ]] ; then
     if getent group "${docker_gid}" ; then
         # The group for the Docker socket's gid already exists.
         echo "Adding user to '$(getent group "${docker_gid}" | cut -d: -f1)' group for docker access."
-        sudo usermod -aG "${docker_gid}" "${MAMBA_USER}"
+        sudo usermod -aG "${docker_gid}" "$(id -u -n)"
     else
         # The group for the Docker socket's gid doesn't exist.
         if getent group docker ; then
@@ -23,7 +23,7 @@ if [[ -S /var/run/docker.sock ]] ; then
         fi
         echo "Setting the GID of the '${docker_group_name}' group to ${docker_gid}."
         sudo groupadd --force --gid "${docker_gid}" "${docker_group_name}"
-        sudo usermod -aG "${docker_group_name}" "${MAMBA_USER}"
+        sudo usermod -aG "${docker_group_name}" "$(id -u -n)"
     fi
   fi
 fi
